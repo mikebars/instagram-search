e297d6e5fb6d0e5407813308b07e56fa16e4afce
mike bars <mikebarsdev@gmail.com>
2016-05-19 03:08:08 -0700

    gh-pages branch

--- source/js/helpers/history.js
+++
@@ -1,7 +0,0 @@
-import { browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
-import store from '../stores/store';
-
-const history = syncHistoryWithStore(browserHistory, store);
-
-export default history;
--- source/js/helpers/utilities.js
+++
@@ -1,3 +0,0 @@
-export function sanitizeTagInput(tag) {
-  return String(tag).replace(/[^0-9a-zA-Z]/gi, '');
-}
